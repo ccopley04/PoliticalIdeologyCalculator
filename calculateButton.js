b22e3464c166474233ca94db9c22b569fdfdab2b
Copley, Caleb Scott <ccopley7@gatech.edu>
2025-07-09 15:39:11 -0400

    Added Government Ideologies
    Added the file GovernmentIdeologies.js that creates an instance of the Binary Tree structure. This tree holds all ideologies that involve government control and personal freedom. Updated the calculate button to show on the screen whichever ideology is the closest to the current value of the slider. Also added the most extreme ideologies to the tree to start with.

--- calculateButton.js
+++ calculateButton.js
@@ -2,10 +2,12 @@ import { getScore } from "./createSliders.js";
 import { searchIdeologies } from "./Ideologies/EncompassingIdeologies.js";
 import { searchEconomicIdeologies } from "./Ideologies/EconomicIdeologies.js";
 import { searchForeignIdeologies } from "./Ideologies/ForeignIdeologies.js";
+import { searchGovernmentIdeologies } from "./Ideologies/GovernmentIdeologies.js";
 
 const calculateButton = document.getElementById("calculate");
 const economicResult = document.getElementById("economicPolicyResult");
 const foreignResult = document.getElementById("foreignPolicyResult");
+const governmentResult = document.getElementById("governmentPolicyResult");
 
 calculateButton.addEventListener("click", () => {
   let currResult = searchEconomicIdeologies(getScore()[0]);
@@ -13,4 +15,8 @@ calculateButton.addEventListener("click", () => {
 
   currResult = searchForeignIdeologies(getScore()[1]);
   foreignResult.textContent = currResult.name + ": " + currResult.description;
+
+  currResult = searchGovernmentIdeologies(getScore()[2]);
+  governmentResult.textContent =
+    currResult.name + ": " + currResult.description;
 });
